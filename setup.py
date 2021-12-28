32a447575e075f6652150ece524838d52c7b989f
pickerln <43866263+pickerln@users.noreply.github.com>
2021-12-28 12:01:34 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,8 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
-setuptools.setup(
-    name='gfs_basic',
-    version='1.0.0',
-    author = 'Lynn Pickering',
-    author_email = 'pickerln@mail.uc.edu',
-    description='Basic genetic fuzzy systems code',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url='https://github.com/pickerln/GFS_basic',
-    project_urls = {
-            "Bug Tracker": "https://github.com/pickerln/gfs_basic/issues"
-        },
-    license='MIT',
-    packages=['gfs_basic'],
+setup(
+    install_requires=requirements,
 )
-
